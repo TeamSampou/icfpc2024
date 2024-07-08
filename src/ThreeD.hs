3b185741ef8c5429a957da2daca9c3f3c221b3d3
cutsea110 <cutsea110@gmail.com>
2024-07-08 14:49:14 +0900

    fix: indent for height less than 10

--- src/ThreeD.hs
+++ src/ThreeD.hs
@@ -318,10 +318,12 @@ drawGame (w, h) g = putStrLn $ showGame (w, h) g
 showGame :: (Int, Int) -> Grid -> String
 showGame (w, h) g = unlines $ header:zipWith (\i l -> showRow i ++ " " ++ l) [0..] body
   where
-    header = intercalate "\n" $ map (("    "++) . intersperse ' ') . transpose $ cols
+    header = intercalate "\n" $ map ((spaces ++) . intersperse ' ') . transpose $ cols
       where
-        len  = length $ show (w-1)
-        cols = map (pad len . show) [0..w-1]
+        hlen = length $ show (h-1)
+        wlen = length $ show (w-1)
+        cols = map (pad wlen . show) [0..w-1]
+        spaces = pad (hlen + 2) " "
     body = map concat grid
     pad :: Int -> String -> String
     pad size s | size >= len = replicate (size - len) ' ' ++ s
